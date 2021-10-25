8221825f5f96fe0ad9a1fad0de506b5ed49fffab
vasimlook <vasimlook@gmail.com>
2021-10-25 10:48:39 +0530

    add name in diamonds
    add name in diamonds

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -160,7 +160,7 @@ final class Versions
   'sebastian/type' => '2.3.4@b8cd8a1c753c90bc1a0f5372170e3e489136f914',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.1@34a41e998c2183e22995f158c581e7b5e755ab9e',
-  'laravel/laravel' => 'dev-main@e497a19ed5ff662ef64f18da17f6956dd659dc61',
+  'laravel/laravel' => 'dev-main@7e1f3b08a2ccb6dbf6f812e65040840e02eebd4e',
 );
 
     private function __construct()
