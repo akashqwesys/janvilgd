b976e9eacf202ac90f8e32329b61422905df1dcd
vasimlook <vasimlook@gmail.com>
2021-10-26 11:55:03 +0530

    changes
    changes

---
+++ database/migrations/2021_10_26_062417_add_image_diamonds.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddImageDiamonds extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('diamonds', function (Blueprint $table) {
+             $table->text('image')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('diamonds', function (Blueprint $table) {
+            //
+        });
+    }
+}
--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -160,7 +160,7 @@ final class Versions
   'sebastian/type' => '2.3.4@b8cd8a1c753c90bc1a0f5372170e3e489136f914',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.1@34a41e998c2183e22995f158c581e7b5e755ab9e',
-  'laravel/laravel' => 'dev-main@61d8a7dafb982c92f22d676f74b7c2f216236b81',
+  'laravel/laravel' => 'dev-main@2ad329609137d0b8b574edf4c89dbff99db43d39',
 );
 
     private function __construct()
