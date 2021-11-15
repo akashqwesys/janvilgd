fe70d298f5f2d167f90cc174c821bfaf7541e876
Sumeet <sgpatel1996@outlook.com>
2021-11-15 17:47:47 +0530

    migrate

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -180,7 +180,7 @@ final class Versions
   'sebastian/type' => '2.3.4@b8cd8a1c753c90bc1a0f5372170e3e489136f914',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.1@34a41e998c2183e22995f158c581e7b5e755ab9e',
-  'laravel/laravel' => 'dev-main@154d34ba40b40025a002bd2076a2bd6473ee6152',
+  'laravel/laravel' => 'dev-main@a1b73f9137cd52ba6d43a8e1e7904b61691cbe96',
 );
 
     private function __construct()
