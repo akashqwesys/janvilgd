00035fd1d8ec662de96586e99b21de37482eaa84
vasimlook <vasimlook@gmail.com>
2021-11-18 12:45:01 +0530

    changes
    changes

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -795,20 +795,20 @@ class CommonController extends Controller
                 $data_array['date_updated']=date("Y-m-d h:i:s");
                 array_push($attribute_groups_array,$data_array);
 
-                $data_array=array();
-                $data_array['name']='EXP POL SIZE';
-                $data_array['image_required']=0;
-                $data_array['field_type']=0;
-                $data_array['refCategory_id']=$row->category_id;
-                $data_array['is_required']=1;
-                $data_array['sort_order']=1;
-                $data_array['added_by']=1;
-                $data_array['is_fix']=1;
-                $data_array['is_active']=1;
-                $data_array['is_deleted']=0;
-                $data_array['date_added']=date("Y-m-d h:i:s");
-                $data_array['date_updated']=date("Y-m-d h:i:s");
-                array_push($attribute_groups_array,$data_array);
+//                $data_array=array();
+//                $data_array['name']='EXP POL SIZE';
+//                $data_array['image_required']=0;
+//                $data_array['field_type']=0;
+//                $data_array['refCategory_id']=$row->category_id;
+//                $data_array['is_required']=1;
+//                $data_array['sort_order']=1;
+//                $data_array['added_by']=1;
+//                $data_array['is_fix']=1;
+//                $data_array['is_active']=1;
+//                $data_array['is_deleted']=0;
+//                $data_array['date_added']=date("Y-m-d h:i:s");
+//                $data_array['date_updated']=date("Y-m-d h:i:s");
+//                array_push($attribute_groups_array,$data_array);
 
                 $data_array=array();
                 $data_array['name']='SHAPE';
