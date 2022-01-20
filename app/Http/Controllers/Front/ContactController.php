2542c01ceba3c0bf5c9d27bda68a7b932aebd99f
vasimlook <vasimlook@gmail.com>
2022-01-20 13:07:20 +0530

    changes
    changes

--- app/Http/Controllers/Front/ContactController.php
+++ app/Http/Controllers/Front/ContactController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use App\Mail\CommonEmail;
