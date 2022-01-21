153e7dbcb107e166eb0d8762f62489c1232cf750
vasimlook <vasimlook@gmail.com>
2022-01-21 12:45:55 +0530

    Update list.blade.php

--- resources/views/admin/contacts/list.blade.php
+++ resources/views/admin/contacts/list.blade.php
@@ -55,7 +55,7 @@
                                             <td> {{ $c->phone }} </td>
                                             <td> {{ $c->email }} </td>
                                             <td> {{ $c->subject }} </td>
-                                            <td> <?php echo wordwrap($c->message,100,"<br>\n"); ?> </td>
+                                            <td> <?php echo wordwrap($c->message,150,"<br>\n"); ?> </td>
                                             <td> {{ $c->date_added }} </td>
                                         </tr>
                                         @endforeach
