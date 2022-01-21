f6ea3a89e1b4b2daf1ed9f6e5e274b195ffb2473
vasimlook <vasimlook@gmail.com>
2022-01-21 13:55:50 +0530

    Update layout_1.blade.php

--- resources/views/front/layout_1.blade.php
+++ resources/views/front/layout_1.blade.php
@@ -442,7 +442,7 @@ function init() {
 	// 	utilsScript: "../../build/js/utils.js?1638200991544" // just for formatting/placeholders etc
 	// });
 	input.addEventListener("countrychange",function() {
-		alert(iti.getSelectedCountryData());
+		alert(iti.getSelectedCountryData().iso2);
 	});
 
   </script>
