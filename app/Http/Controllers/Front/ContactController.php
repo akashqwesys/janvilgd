432270658acd8d89ca01ef98436ef67bcc64361f
Sumeet <sgpatel1996@outlook.com>
2022-02-15 12:55:25 +0530

    contact page

--- app/Http/Controllers/Front/ContactController.php
+++ app/Http/Controllers/Front/ContactController.php
@@ -16,18 +16,16 @@ class ContactController extends Controller {
 
     public function index(Request $request)
     {
-        if(isset($request->txt_name)) { 
+        if(isset($request->txt_name)) {
 
-            $request->txt_phone=$request->txt_phone['full'];
+            // $request->txt_phone=$request->txt_phone['full'];
 
             $rules = [
                 'txt_name' => ['required'],
                 'txt_phone' => ['required'],
                 'txt_email' => ['required', 'email'],
                 'txt_msg' => ['required']
-            ];           
-
-            
+            ];
 
             $message = [];
 
@@ -44,31 +42,31 @@ class ContactController extends Controller {
             $data_array['subject']=$request->txt_subject;
             $data_array['message']=$request->txt_msg;
             $data_array['date_added']=date('Y-m-d H:i:s');
-            
-            DB::table('contact_req')->insert($data_array);           
-            // $admin_email = DB::table('settings')
-            //     ->select('value')
-            //     ->where('key', 'admin_email')
-            //     ->pluck('value')
-            //     ->first();
-            // Mail::to($admin_email)
-            //     ->send(
-            //         new CommonEmail([
-            //             'subject' => 'Inquiry from Janvi LGE',
-            //             'data' => [
-            //                 'time' => date('Y-m-d H:i:s'),
-            //                 'name' => $request->txt_name,
-            //                 'phone' => $request->txt_phone,
-            //                 'email' => $request->txt_email,
-            //                 'subject' => $request->txt_subject,
-            //                 'msg' => $request->txt_msg,
-            //             ],
-            //             'view' => 'emails.inquiryEmail'
-            //             ])
-            //         );
-            // return back()->with(['success' => 1, 'message' => 'Inquiry sent successfully']);
+
+            DB::table('contact_req')->insert($data_array);
+            $admin_email = DB::table('settings')
+                ->select('value')
+                ->where('key', 'admin_email')
+                ->pluck('value')
+                ->first();
+            Mail::to($admin_email)
+                ->send(
+                    new CommonEmail([
+                        'subject' => 'Inquiry from Janvi LGE',
+                        'data' => [
+                            'time' => date('Y-m-d H:i:s'),
+                            'name' => $request->txt_name,
+                            'phone' => $request->txt_phone,
+                            'email' => $request->txt_email,
+                            'subject' => $request->txt_subject,
+                            'msg' => $request->txt_msg,
+                        ],
+                        'view' => 'emails.inquiryEmail'
+                        ])
+                    );
+            return back()->with(['success' => 1, 'message' => 'Inquiry sent successfully']);
             // return redirect('/contact')->with(['success' => 1, 'message' => 'Inquiry sent successfully']);
-            successOrErrorMessage("Inquiry sent successfully", 'success');
+            // successOrErrorMessage("Inquiry sent successfully", 'success');
         }
         return redirect('/contact');
     }
